300bab324679651dcdbe2c413eb66c772cee5c25
One <pavel.svitsenka@gmail.com>
2024-08-19 14:12:13 +0300

    Смена позиций игроков

--- stack_player.py
+++ stack_player.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 
 class Player:
-    def __init__(self, player_name: str, stack: float, position: Optional[str] = None):
+    def __init__(self, player_name: Optional[str], stack: float, position: Optional[str] = None):
         self.name = player_name
         self.stack = stack
         self.position = position
--- test_action.py
+++ test_action.py
@@ -1,5 +1,3 @@
-from typing import Dict
-
 from stack_player import Player
 
 POSITION_MAP = {
@@ -20,53 +18,47 @@ BOARD_MAP = {
     'BTN': 6
 }
 
-# positions = ['BB', 'SB', 'BTN', 'CO', 'MP', 'UTG']
-positions = [0, 1, 2, 3, 4, 5]
-round_finished = False
-
 num_active_players = 6
 players = []
+positions = [0, 1, 2, 3, 4, 5]
+round_finished = True
 
+
+# Инициализация игроков
 for i in range(num_active_players):
-    player_name = f"Player_{i + 1}"
-
-    # Запрашиваем бай-ин у пользователя
-    # buy_in = float(input(f"Выберите бай-ин для {player_name} от 40 до 100бб: "))
     buy_in = 100
-    # position = positions[i % len(positions)]
-    position = positions[i]
-
-    player = Player(player_name, stack=buy_in, position=POSITION_MAP[position])
-
+    player_name = f"Player_{i + 1}"
+    player = Player(player_name, stack=buy_in)
     players.append(player)
 
-
-# Вывод информации о всех игроках
 for player in players:
-    if player.position == 'BB':
-        player.stack = player.stack - 1
-    elif player.position == 'SB':
-        player.stack = player.stack - 0.5
-    print(f"Стек игрока {player.name}: {player.stack} бб, позиция: {player.position}")
+    print(f"Стек игрока {player.name}: {player.stack} бб")
 
-total_positions = num_active_players
-print(f"total_positions = {total_positions}")
+print("\n")
 
-
+# Вывод информации об игроках в начале каждой раздачи
 for i in range(num_active_players):
-
-    round_finished = True
-    print("НОВАЯ РАЗДАЧА: ")
-
     if round_finished:
         positions = [(i + 1) % num_active_players for i in positions]
+        print("НОВАЯ РАЗДАЧА: ")
+    # Запрашиваем бай-ин у пользователя
+    # buy_in = float(input(f"Выберите бай-ин для {player_name} от 40 до 100бб: "))
 
-    print(positions, "\n")
+    # print(f"positions = {positions}")
 
+    for idx, pos in enumerate(positions):
+        players[idx].position = POSITION_MAP[pos]
+
+    for player in players:
+        if player.position == 'BB':
+            player.stack = player.stack - 1
+        elif player.position == 'SB':
+            player.stack = player.stack - 0.5
+        print(f"Стек игрока {player.name}: {player.stack} бб, позиция: {player.position}")
+    print("\n")
 
 
 # TODO условия хода на префлопе - у кого старше индекс позиции, тот ходит первым
 # TODO условия хода после префлопа - первый ходит малый блайнд (SB), далее BB и остальные позиции в порядке
 # TODO их индексов. КРОМЕ ХЕДЗ-АПА. Там первый ходит ББ!!!
 # TODO создать словари для позиции ПРЕФЛОП и ПОСЛЕ ПРЕФЛОПА!!!
-
